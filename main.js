bf516bd9524b75dafa652b90850bb9721557d270
iahuang <ianhuang46@gmail.com>
2017-03-29 11:21:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,14 +3,4 @@ function doThing() {
 
 	tbox.innerHTML = "Test 2";
 }
-function typewriterText(string,tbox){
-    tbox.innerHTML+=string
-}
-function onStart() {
-    var tbox = document.getElementById("canvas_main");
-    var text = "This is my English Creative Writing Assignment";
-    for (var i=0;i<text.length;i++) {
-        setTimeout(typewriterText(text[i],tbox),10);
 
-    }
-}
