0f0ba6078d2e50d13cfa9187efacd22c475486e0
iahuang <ianhuang46@gmail.com>
2017-03-29 11:18:50 -0400

    fixed var bug

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function typewriterText(string,tbox){
 function onStart() {
     var tbox = document.getElementById("canvas_main");
     var text = "This is my English Creative Writing Assignment";
-    for (int i=0;i<text.length;i++) {
+    for (var i=0;i<text.length;i++) {
         setTimeout(typewriterText(text[i],tbox),10);
 
     }
