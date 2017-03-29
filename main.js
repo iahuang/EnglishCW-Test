4b698bd6074a2724f5fbaa1d9539ffc3800dbfd1
iahuang <ianhuang@Bag-End.local>
2017-03-29 15:16:20 -0400

    commit

--- main.js
+++ main.js
@@ -4,13 +4,34 @@ function doThing() {
 	tbox.innerHTML = "Loadisng...";
 }
 function typewriterText(string,tbox){
-    tbox.innerHTML+=string
+    tbox.innerHTML+=string;
+}
+function sleep(milliseconds) {
+ 	var start = new Date().getTime();
+  	for (var i = 0; i < 1e7; i++) {
+    	if ((new Date().getTime() - start) > milliseconds){
+      		break;
+    	}
+  	}
+}
+function type() {
+    text = str.slice(0, ++i);
+    if (text === str) return;
+
+    document.getElementById('').innerHTML = text;
+
+    var char = text.slice(-1);
+    if( char === '<' ) isTag = true;
+    if( char === '>' ) isTag = false;
+
+    if (isTag) return type();
+    setTimeout(type, 80);
 }
 function onStart() {
     var tbox = document.getElementById("canvas_main");
     var text = "This is my English Creative Writing Assignment";
-    for (int i=0;i<text.length;i++) {
-        setTimeout(typewriterText(text[i],tbox),10);
 
-    }
+	for (var i=0;i<text.length;i++) {
+		tbox.innerHTML+=text[i];
+	}
 }
