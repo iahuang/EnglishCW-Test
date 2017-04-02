e77abb053d20fa69ac3a8a787ee4556a6619729b
iahuang <ianhuang@Bag-End.local>
2017-04-02 12:37:57 -0400

    Cookies!
    No, the website doesn’t use cookies, but there are cookies included
    however.

--- main.js
+++ main.js
@@ -32,6 +32,9 @@ function _write(text) {
 function writeToBoard(text) {
 	outputQueue.push(text);
 }
+function newLine() {
+	outputQueue.push(" ");
+}
 function onNext() { // When "Next" button is pressed in intro sequence
 	if (!outputDone) {
 		return;
@@ -51,7 +54,66 @@ function onNext() { // When "Next" button is pressed in intro sequence
 	charIndex = 0;
 	tbox.innerHTML = "";
 }
+function isIn(array,value) {
+	for (var i=0;i<array.length;i++) {
+		if (array[i] == value) {
+			return true;
+		}
+	}
+	return false;
+}
+function removeFromArray(array, value) {
+	var index = array.indexOf(value);
+	if (index > -1) {
+		array.splice(value,1);
+	}
+}
+function getGameObject(room,name) {
+	for (var i=0;i<room.objects.length;i++) {
+		var obj = room.objects[i];
+		if (isIn(obj.synonyms,name)) {
+			return obj;
+		}
+	}
+	return undefined;
+}
+function actionGeneric(actionName,onDo,input) {
+	if (isIn(actions[actionName],input[0])) {
+		var lastWord = input[input.length-1];
+		var selectedObj = getGameObject(rooms[currentRoom],lastWord);
+
+		if (selectedObj == undefined) {
+			writeToBoard("There is no "+lastWord+" in this area.");
+			return true;
+		} else {
+			onDo(selectedObj);
+			return true;
+		}
+	}
+	return false;
+}
+function onActionExamine(obj) {
+	writeToBoard(obj.description);
+}
+function onActionEat(obj) {
+	writeToBoard("You ate the "+obj.synonyms[0]);
+	removeFromArray(rooms[currentRoom].objects,obj);
+}
+function onActionTake(obj) {
+	writeToBoard("You took the "+obj.synonyms[0]);
+	removeFromArray(rooms[currentRoom].objects,obj);
+}
 function onConfirm() {
+	var input = ibox.value.toLocaleLowerCase().split(" ");
+	var lastWord = input[input.length-1];
+	if (isIn(input,"please")) {
+		writeToBoard("How polite of you. However, it is recommended that you make your actions as concise as possible.");
+	} else if (actionGeneric("examine",onActionExamine,input)) {
+	} else if (actionGeneric("eat",onActionEat,input)) {
+	} else if (actionGeneric("take",onActionTake,input)) {
+	} else {
+		writeToBoard("You cannot do that here.");
+	}
 	ibox.value = "";
 
 }
@@ -86,9 +148,10 @@ class Action {
 	}
 }
 class GameObject { // Class for all objects able to be interacted with
-	constructor(synonyms,actions) {
+	constructor(synonyms,roomDescriptions,description) {
 		this.synonyms = synonyms;
-		this.onUse = onUse;
+		this.description = description;
+		this.roomDescriptions = roomDescriptions;
 	}
 }
 class Room {
@@ -96,9 +159,31 @@ class Room {
 		this.objects = objects;
 		this.description = description;
 	}
+	getId() {
+		for (var room in rooms) {
+			if (this.description == rooms[room].description) {
+				return room;
+			}
+		}
+	}
+	display() {
+		writeToBoard(this.description);
+
+		for (var i=0;i<this.objects.length;i++) {
+			var object = this.objects[i];
+			writeToBoard(object.roomDescriptions[this.getId()]);
+		}
+	}
+
 }
 
-var rooms = {"test_room":new Room([],"We are number one")};
+var rooms = {"test_room":new Room([
+	new GameObject(["cookie","biscuit"],
+	{"test_room":"There is a cookie sitting on a table"},"It is an oatmeal-raisin cookie")
+],"Welcome to the testroom.&nIt's like a restroom, but for testing.")};
+var actions = {"examine":["examine","look","check","inspect"],
+"eat":["eat","consume"],
+"take":["take","grab","steal","pocket"]};
 var tbox = document.getElementById("canvas_main");
 var ibox = document.getElementById("action_input");
 var idiv = document.getElementById("input_div");
@@ -113,8 +198,11 @@ var outputQueue = [];
 var outputDone = true;
 var introIndex = 0;
 var charIndex = 0;
+
+currentRoom = "test_room";
+
 setInterval(function(){onUpdate();},40);
 setInterval(function(){_write();},10);
 //writeToBoard("Introduction");
 gameInit();
-writeToBoard(rooms["test_room"].description);
+rooms["test_room"].display();
