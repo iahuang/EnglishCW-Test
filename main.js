32c8485ef1587192652b6e3e9f7e6bb0abb0a54d
iahuang <ianhuang@Bag-End.local>
2017-04-03 17:36:50 -0400

    Added tutorial!

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function gameInit() { // Disable the "Next" button and initialize game
 	nextButton.disabled = true;
 	nextButton.innerHTML = "";
 	idiv.style.visibility = 'visible';
-	writeToBoard("Starting Game...&n");
+	writeToBoard("Starting Game...&nType help at anytime for a list of useful actions&n");
 }
 function _write(text) {
 	if (outputDone) {
@@ -62,6 +62,13 @@ function isIn(array,value) {
 	}
 	return false;
 }
+function getIn(array,value) {
+	for (var i=0;i<array.length;i++) {
+		if (array[i] == value) {
+			return array[i];
+		}
+	}
+}
 function removeFromArray(array, value) {
 	var index = array.indexOf(value);
 	if (index > -1) {
@@ -77,41 +84,147 @@ function getGameObject(room,name) {
 	}
 	return undefined;
 }
-function actionGeneric(actionName,onDo,input) {
-	if (isIn(actions[actionName],input[0])) {
-		var lastWord = input[input.length-1];
-		var selectedObj = getGameObject(rooms[currentRoom],lastWord);
+function getGameObjectFromInventory(name) {
+	for (var i=0;i<inventory.length;i++) {
+		var obj = inventory[i];
+		if (isIn(obj.synonyms,name)) {
+			return obj;
+		}
+	}
+	return undefined;
+}
+function actionGeneric(actionName,onDo,input,useInventory) {
+	var lastWord = input[input.length-1];
+	var selectedObj;
 
+	if (isIn(actions[actionName],input[0])) {
+		if (useInventory != undefined) {
+			console.log(inventory);
+			console.log(lastWord);
+			selectedObj = getGameObjectFromInventory(lastWord);
+		}
 		if (selectedObj == undefined) {
-			writeToBoard("There is no "+lastWord+" in this area.");
+			selectedObj = getGameObject(rooms[currentRoom],lastWord);
+		}
+		if (selectedObj == undefined) {
+			writeToBoard("Could not find: "+lastWord);
 			return true;
 		} else {
 			onDo(selectedObj);
 			return true;
 		}
 	}
+
 	return false;
+}
+function actionGo(input) {
+	if (!isIn(actions["go"],input[0])) {
+		return false;
+	}
+	if (tutorialLimiter(3)) {
+		return true;
+	}
+	var direction = -1;
+	var directions = [
+		['left','l'],
+		['forward','f','fwd'],
+		['right','r'],
+		['back','backwards','b']
+	];
+
+	var proposedDirection = input[input.length-1];
+
+	for (var i=0;i<directions.length;i++) {
+		if (isIn(directions[i],proposedDirection)) {
+			if (rooms[currentRoom].connected[i] != undefined) {
+				loadRoom(rooms[currentRoom].connected[i]);
+				return true;
+			}
+		}
+	}
+	writeToBoard("You cannot go that way.");
+	return true;
 }
 function onActionExamine(obj) {
 	writeToBoard(obj.description);
 }
-function onActionEat(obj) {
+function tutorialLimiter(state) {
+	if (currentRoom == "test_room") {
+		if (tutorialState < state) {
+			writeToBoard("Don't do that yet.");
+			return true;
+		}
+	}
+	return false;
+}
+function onActionEat(obj,useInventory) {
+	if (tutorialLimiter(2)) {
+		return;
+	}
 	writeToBoard("You ate the "+obj.synonyms[0]);
 	removeFromArray(rooms[currentRoom].objects,obj);
+	removeFromArray(inventory,obj);
+	if (currentRoom == "test_room") {
+		writeToBoard("Great. Now you are finished with the tutorial.&nYou can exit through the door on your right by typing 'go right' or simply 'go r'");
+		tutorialState = 3;
+	}
 }
 function onActionTake(obj) {
+	if (tutorialLimiter(1)) {
+		return;
+	}
 	writeToBoard("You took the "+obj.synonyms[0]);
+	inventory.push(obj);
 	removeFromArray(rooms[currentRoom].objects,obj);
+	if (currentRoom == "test_room" && tutorialState == 1) {
+		tutorialState = 2;
+		writeToBoard("Good job.&nThe cookie has been added to your inventory.&nYou may check your inventory at any time\
+		by typing 'inventory'.&nNow, as a reward, you can eat the cookie by typing 'eat cookie'.&s Five second rule, right?");
+	}
+}
+function onActionThrow(obj,useInventory) {
+	writeToBoard("You threw the "+obj.synonyms[0]);
+	removeFromArray(inventory,obj);
+	if (currentRoom == "test_room" && tutorialState == 0) {
+		tutorialState = 1;
+		writeToBoard("Good job. Now pick the cookie off the floor by typing 'pick up cookie', 'take cookie' etc.");
+	}
+}
+function loadRoom(roomId) {
+	currentRoom = roomId;
+	rooms[currentRoom].display();
 }
 function onConfirm() {
+	if (ibox.value == "") {
+		return;
+	}
 	var input = ibox.value.toLocaleLowerCase().split(" ");
 	var lastWord = input[input.length-1];
 	if (isIn(input,"please")) {
 		writeToBoard("How polite of you. However, it is recommended that you make your actions as concise as possible.");
+	} else if (actionGo(input)) {
+	} else if (lastWord == "inventory") {
+		if (inventory.length == 0) {
+			writeToBoard("There is nothing in your inventory.");
+		} else {
+			writeToBoard("Inventory:");
+			newLine();
+			for (var i=0;i<inventory.length;i++) {
+				var item = inventory[i];
+				writeToBoard("- "+item.inventoryDescription);
+			}
+		}
 	} else if (actionGeneric("examine",onActionExamine,input)) {
-	} else if (actionGeneric("eat",onActionEat,input)) {
+	} else if (actionGeneric("eat",onActionEat,input,true)) {
 	} else if (actionGeneric("take",onActionTake,input)) {
-	} else {
+	} else if (actionGeneric("throw",onActionThrow,input,true)) {
+	} else if (input[0] == "help") {
+		writeToBoard("Useful actions you can do at any time:&n&n\
+		- Inventory: List all items in your inventory&n\
+		- Clear: Clear all text");
+
+	}
+	else {
 		writeToBoard("You cannot do that here.");
 	}
 	ibox.value = "";
@@ -121,7 +234,7 @@ function onUpdate() {
 	if (currentOutput == "") {
 		return;
 	}
-	currentOutput = currentOutput.replace("&s","&&&&&");
+	currentOutput = currentOutput.replace("&s","&&&");
 	if (charIndex < currentOutput.length) {
 		if (currentOutput[charIndex] == "&") {
 			var nextChar = currentOutput[charIndex+1];
@@ -141,23 +254,20 @@ function onUpdate() {
 	}
 
 }
-class Action {
-	constructor(synonyms,onUse) {
-		this.synonyms = synonyms;
-		this.onUse = onUse;
-	}
-}
+
 class GameObject { // Class for all objects able to be interacted with
-	constructor(synonyms,roomDescriptions,description) {
+	constructor(synonyms,roomDescriptions,description,inventoryDescription) {
 		this.synonyms = synonyms;
 		this.description = description;
 		this.roomDescriptions = roomDescriptions;
+		this.inventoryDescription = inventoryDescription;
 	}
 }
 class Room {
-	constructor(objects,description) {
+	constructor(objects,description,connected) {
 		this.objects = objects;
 		this.description = description;
+		this.connected = connected;
 	}
 	getId() {
 		for (var room in rooms) {
@@ -176,14 +286,21 @@ class Room {
 	}
 
 }
-
+var objectCookie = new GameObject(["cookie","biscuit"],
+{"test_room":"There is a cookie sitting on a table.&s First, try throwing the cookie across the room. &n\
+You can do this by typing 'throw the cookie' or simply 'throw cookie'."},"It is an oatmeal-raisin cookie",
+"A generic cookie");
 var rooms = {"test_room":new Room([
-	new GameObject(["cookie","biscuit"],
-	{"test_room":"There is a cookie sitting on a table"},"It is an oatmeal-raisin cookie")
-],"Welcome to the testroom.&nIt's like a restroom, but for testing.")};
-var actions = {"examine":["examine","look","check","inspect"],
+	objectCookie
+],"Welcome to the tutorial.&nThis is an introduction to the way \
+you are able to interact with your enviroment.",[undefined,undefined,"start_room",undefined]),
+"start_room":new Room([],"Test.",[undefined,undefined,undefined,undefined])};
+var actions = {"examine":["examine","look","check","inspect","read"],
 "eat":["eat","consume"],
-"take":["take","grab","steal","pocket"]};
+"take":["take","grab","steal","pocket","pick"],
+"throw":["throw","hurl","chuck","toss"],
+"go":["go","move"]};
+var inventory = [];
 var tbox = document.getElementById("canvas_main");
 var ibox = document.getElementById("action_input");
 var idiv = document.getElementById("input_div");
@@ -199,10 +316,12 @@ var outputDone = true;
 var introIndex = 0;
 var charIndex = 0;
 
+var tutorialState = 0;
+
 currentRoom = "test_room";
 
-setInterval(function(){onUpdate();},40);
+setInterval(function(){onUpdate();},30);
 setInterval(function(){_write();},10);
 //writeToBoard("Introduction");
 gameInit();
-rooms["test_room"].display();
+loadRoom(currentRoom);
